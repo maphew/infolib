87c39177e0763af948b3184766a561a6f77a894c
maphew <maphew@localhost>
2010-11-09 12:25:50 -0800

    small helper batch file to recall list of command line utilities

---
+++ infolib.bat
@@ -0,0 +1,10 @@
+@echo off
+echo.
+echo	-=[ INFOLIB.BAT ]=-
+echo.
+echo 	AVAILABLE INFOLIB COMMANDS (recall by typing "infolib")
+echo.
+echo 	ascii2info   dbf2info     dbfitems     dbflist
+echo 	dbflook      info2ascii   info2dbf     infodel
+echo 	infodir      infoitems    infolist     infolook
+echo.
